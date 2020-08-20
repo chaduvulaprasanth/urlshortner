69fbaeeba24329db68d401442c83d643a7c55360
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-20 19:10:08 +0530

    added functionality and view of url lists

---
+++ app/javascript/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav className="wrapper">
+          <a className="logo" href="/">
+            Ur.ly
+          </a>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
