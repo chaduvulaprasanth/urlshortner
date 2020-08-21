64d0a10e27485bb34e98c61ca39927841ab6b4e0
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-08-21 11:56:33 +0530

    generated report in table view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
 
   def generate_report(email)
+    @urls = Url.order("pinned DESC, created_at DESC")
     mail to: email, subject: "Generate Report"
   end
 end
